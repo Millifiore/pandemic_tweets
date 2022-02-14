d6b2ad75de6ad1f1e14428e78bbaaa085aaea933
Nina Mezu <ninabina921@users.noreply.github.com>
2022-02-13 22:03:03 -0500

    Add routing to app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder='static')
 
 @app.route("/")
 def hello_world():
@@ -8,16 +8,16 @@ def hello_world():
 
 # About Page Route
 @app.route("/about")
-def index():
+def home():
     return render_template("index.html")
 
 # Demo Input Route
 @app.route("/demo-output")
-def index():
+def demo():
     return render_template("demo-output.html")
 
 # Demo Output Route
 @app.route("/demo-input")
-def index():
+def input():
     return render_template("demo-input.html")
 
