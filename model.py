a33d2f3e2d2cb3d90efb1b5e2eb6ce7a0ba92c01
Xavier Ray <xaray1@morgan.edu>
2022-02-24 20:37:06 -0500

    Fixed bug and added rumordata

--- model.py
+++ model.py
@@ -8,66 +8,59 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 warnings.filterwarnings('ignore')
 
-## IMPORTING THE DATASET
+# IMPORTING THE DATASET
 
-df = pd.read_csv('rumor_data/rumordata_cleaned.csv', delimiter=',',skiprows=1, header=None, names=['label','content']) #Read in the data, skipping the labels in the first row.
-pos_label = df[df['label']==1] #Grab the data labeled 1 and slice 500 of them
+df = pd.read_csv('rumor_data/rumordata_cleaned.csv', delimiter=',', skiprows=1, header=None, names=['label', 'content'])  # Read in the data, skipping the labels in the first row.
+pos_label = df[df['label'] == 1]  # Grab the data labeled 1 and slice 500 of them
 pos_label = pos_label[:500]
-neg_label = df[df['label']==0] #Do the same thing above for data labeled 0
+neg_label = df[df['label'] == 0]  # Do the same thing above for data labeled 0
 neg_label = neg_label[:500]
-
-covid_dataset = pd.concat([pos_label,neg_label]).reset_index(drop=True) #Concat positive and negative labels and reset the index
+covid_dataset = pd.concat([pos_label, neg_label]).reset_index(drop=True)  # Concat pos/neg labels and reset the index
 print(covid_dataset['label'].value_counts())
 
-## LOADING THE PRE-TRAINED BERT MODEL
+# LOADING THE PRE-TRAINED BERT MODEL
+model_class = ppb.DistilBertModel  # This is the BERT model we are using. DistilBERT
+tokenizer_class = ppb.DistilBertTokenizer  # Tokenizer used for DistilBERT
+pretrained_weights = 'distilbert-base-cased'  # The is what we are using to pretrain BERT
+tokenizer = tokenizer_class.from_pretrained(pretrained_weights)  # Load the pretrained tokenizer
+model = model_class.from_pretrained(pretrained_weights) # Load the pretrained model
 
-model_class = ppb.DistilBertModel #This is the BERT model we are using. DistilBERT
-tokenizer_class = ppb.DistilBertTokenizer #Tokenizer used for DistilBERT
-pretrained_weights = 'distilbert-base-cased' #The is what we are using to pretrain BERT
 
-tokenizer = tokenizer_class.from_pretrained(pretrained_weights) #Load the pretrained tokenizer
-model = model_class.from_pretrained(pretrained_weights) #Load the pretrained model
-
-## PREPROCESSING THE DATA FOR DISTILBERT
-
+# PREPROCESSING THE DATA FOR DISTILBERT
 def pre_proc(series, tokenizer):  # Function for preprocessing data for encoding
     tokenized = series.apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))  # Tokenizes series
-
-    max_len = 0
-    for i in tokenized.values:  # Finds the max len of the sentences
+    max_len = 0  # Finds the max len of the sentences
+    for i in tokenized.values:
         if len(i) > max_len:
             max_len = len(i)
-
     padded = np.array([i + [0] * (max_len - len(i)) for i in tokenized.values])  # Pads all list to the same size
     print(np.array(padded).shape)
-
     attention_mask = np.where(padded != 0, 1, 0)  # Adds masking to tell it to ignore the padding
     print(attention_mask.shape)
-
     return padded, attention_mask
 
 
-padded, attention_mask = pre_proc(covid_dataset['content'], tokenizer)  # Pre processes the covid_dataset
-
-##  ENCODING WITH DISTILBERT
-
-def encode(model, attention_mask, padded): #Function that runs the bert model and encodes the data
-  input_ids = torch.tensor(padded)  #creates input ids
-  attention_mask = torch.tensor(attention_mask) #applies the attention mask
-
-  with torch.no_grad(): #Runs the model
-    last_hidden_states = model(input_ids, attention_mask=attention_mask) #Pulls out the encoding we need
-
-  features = last_hidden_states[0][:,0,:].numpy() #Creates features out of the encoding
-
-  return features
+padded, attention_mask = pre_proc(covid_dataset['content'], tokenizer)  # Pre-processes the covid_dataset
 
 
-features = encode(model, attention_mask, padded) #Creates features out of encode()
+#  ENCODING WITH DISTILBERT
+def encode(model, attention_mask, padded): # Function that runs the bert model and encodes the data
+    input_ids = torch.tensor(padded)  # Creates input ids
+    attention_mask = torch.tensor(attention_mask) # Applies the attention mask
 
-## LOGISTIC REGRESSION
+    with torch.no_grad():  # Runs the model
+        last_hidden_states = model(input_ids, attention_mask=attention_mask)  # Pulls out the encoding we need
 
-def log_reg(features, series):  # Runs our classficiation model
+    features = last_hidden_states[0][:,0,:].numpy()  # Create features out of the encoding
+
+    return features
+
+
+features = encode(model, attention_mask, padded)  # Create features out of encode()
+
+
+# LOGISTIC REGRESSION
+def log_reg(features, series):  # Runs our classification model
     labels = series
     train_features, test_features, train_labels, test_labels = train_test_split(features, labels)
 
