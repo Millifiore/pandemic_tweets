6e999c4b41d1e19ac09e33ae178108f40640aac6
Nina Mezu <ninabina921@users.noreply.github.com>
2022-03-08 20:24:21 -0500

    add back twint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, make_response
 from twint_integrate.twint_search import search
 from twint_wordcloud.wordcloud_html import word_cloud
-# from predict_tweets.model import twint_parse
+from predict_tweets.model import twint_parse
 from predict_tweets.predictedData import predictedTweets
 
 
@@ -37,7 +37,7 @@ def demo_input():
 @app.route("/demo-output")
 def demo_output():
     wc, row_count = word_cloud('userTweets.csv')
-    # twint_parse('userTweets.csv')
+    twint_parse('userTweets.csv')
     predict, df = predictedTweets("predictedTweets.csv")
     return render_template("demo-output.html", title=title, footer=footer, wc=wc, tweets= row_count, misinfo= predict["misinfo"], handle = predict["handle"], tables=[df.to_html(index=False,classes=["data", "mystyle"])], titles=df.columns.values )
 
