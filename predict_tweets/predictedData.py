2cbe61101cae5fc68e1914a76168dd873e93dee4
Isis Moran <imoran@mail.ccsf.edu>
2022-03-26 11:36:50 -0700

    edited output page and deleted unused code

--- predict_tweets/predictedData.py
+++ predict_tweets/predictedData.py
@@ -1,21 +1,20 @@
-# importing pandas as pd
 import pandas as pd
 
 def predictedTweets(csv):
     with open(csv, encoding="utf8", errors='ignore') as f:
             df = pd.read_csv(f)
     # Get Values for Prediction Results
+    df["Tweet Content"] = df["tweet"]
+    df[" "] = df["Unnamed: 0"]
+    for i in df[" "]:
+        df[" "][i] = df[" "][i] + 1
     print("Shape", df.shape)
-    cols = ["username", "tweet", "label"]
+    cols = [" ","Tweet Content"]
     df_small = df[cols]
-    df_small = df_small[df_small["label"] == 1]
 
     # Misinformation Dict for Data
     prediction_data = {}
-    
     prediction_data['misinfo'] = df_small.shape[0]
     prediction_data['handle'] = df["username"][0]
-    
-#     print(df_small)
 
     return prediction_data, df_small
