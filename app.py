71f7bf6ad969d8faa8e90489b3988da1cdb91bb9
Isis Moran <imoran@mail.ccsf.edu>
2022-03-26 11:36:16 -0700

    added subtitle and title

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request
 from twint_integrate.twint_search import search
 from twint_wordcloud.wordcloud_html import word_cloud, row_count
 from predict_tweets.model import twint_parse
@@ -7,7 +7,8 @@ from predict_tweets.predictedData import predictedTweets
 
 app = Flask(__name__, template_folder="templates", static_folder='static')
 
-title = "detecting COVID-19 misinformation in text-based social media posts."
+title = "BCC ML"
+subtitle = "BERT Catches COVID using Machine Learning"
 footer = "This Single Page Application is powered by Flask and JQuery"
 
 @app.route("/", methods=["GET", "POST"])
@@ -25,12 +26,12 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("index.html", title=title, footer=footer)
+    return render_template("index.html", title=title, subtitle=subtitle, footer=footer)
 
 # Demo Input Route
 @app.route("/demo-input")
 def demo_input():
-    return render_template("demo-input.html", title=title, footer=footer)
+    return render_template("demo-input.html", title=title, subtitle=subtitle, footer=footer)
 
 # Demo Output Route
 @app.route("/demo-output")
@@ -39,7 +40,7 @@ def demo_output():
     wc = word_cloud("predictedTweets.csv")
     twint_parse('userTweets.csv')
     predict, df = predictedTweets("predictedTweets.csv")
-    return render_template("demo-output.html", title=title, footer=footer, wc=wc, tweets= rowcount, misinfo= predict["misinfo"], handle = predict["handle"], tables=[df.to_html(index=False,classes=["data", "mystyle"])], titles=df.columns.values )
+    return render_template("demo-output.html", title=title, subtitle=subtitle, footer=footer, wc=wc, tweets=rowcount, misinfo=predict["misinfo"], handle=predict["handle"], html=df.to_html(index=False, classes=["data", "mystyle"]))  
 
 if __name__ == '__main__':
     app.run(debug=True)
